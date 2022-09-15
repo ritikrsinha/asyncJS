a9abb5956d9c5b856bc273f28d8e6773d1bf4e67
ritik sinha <ritikxsinha@gmail.com>
2022-09-15 21:32:33 +0530

    callback

--- callbacks.js
+++ callbacks.js
@@ -1,13 +1,16 @@
 const posts = [
-    {title: 'Post One', body: 'This is post one'},
-    {title: 'Post Two', body: 'This is post two'}
+    {title: 'Post One', body: 'This is post one', createdAt: new Date().getTime()},
+    {title: 'Post Two', body: 'This is post two', createdAt: new Date().getTime()}
 ];
 
+let intervalId = 0;
+
 function getPosts(){
-    setTimeout(() => {
+    clearInterval(intervalId);
+    intervalId = setInterval(() => {
         let output = '';
         posts.forEach((post, index) => {
-            output += `<li>${post.title}</li>`
+            output += `<li>${post.title} - last updated ${(new Date().getTime() - post.createdAt) / 1000}  seconds ago </li>`
         });
         document.body.innerHTML = output;
     }, 1000);
@@ -15,18 +18,31 @@ function getPosts(){
 
 function createPosts(post, callback) {
     setTimeout(()=>{
-        posts.push(post);
+        posts.push({...post, createdAt: new Date().getTime()});
+
         callback();
     },2000);
 }
 
 function create4thPost(post, callback){
     setTimeout(() => {
-        posts.push(post)
+        posts.push({...post, createdAt: new Date().getTime()})
         callback();
     }, 4000)
 }
 
+getPosts();
 createPosts({title: 'Post Three', body: 'This is post three'}, getPosts);
 
 create4thPost({title: 'Post Four', body: 'This is post Four'}, getPosts);
+
+var timer;
+var count = 0;
+function lastEditedInSecondsAgo(){
+    count = 0;
+    clearInterval(timer)
+    timer = setInterval(() => {
+        count++
+        console.log(count)
+    }, 2000);
+}
